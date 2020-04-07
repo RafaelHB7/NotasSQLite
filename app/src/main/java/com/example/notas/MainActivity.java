2599d42337e46bef591c9810b473404c38b5a190
RafaelHB7 <rafael.hobi9@gmail.com>
2020-04-07 14:03:22 -0300

    Tudo funcionando

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -11,21 +11,39 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Notas nota;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.editText = findViewById(R.id.editText);
+
+        nota = new Notas(this);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Nota salva com sucesso!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                nota.salvaNota(editText.getText().toString());
             }
         });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        editText.setText(nota.recuperaNota());
+    }
+
+    protected void onPause() {
+        super.onPause();
+        nota.salvaNota(editText.getText().toString());
     }
 }
